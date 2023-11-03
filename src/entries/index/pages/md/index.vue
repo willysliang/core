b71fa5c1d68f4208458d3d0d2c55da43d4d80e28
梁伟历 <wb.liangweili@mesg.corp.netease.com>
2023-11-03 12:00:42 +0800

    feat: 样式提取 & blog首页

--- src/entries/index/pages/md/index.vue
+++ src/entries/index/pages/md/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    bbbbbbbbbbbbbbbbbbbbbbbbb
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
 </template>
---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+// 声明下述文件为模块，不然在typescript-eslint无法识别
+declare module '*.png'
+declare module '*.gif'
+declare module '*.jpg'
+declare module '*.webp'
+declare module '*.apng'
+declare module '*.svga'
+declare module '*.avif'
+declare module '*.scss'
