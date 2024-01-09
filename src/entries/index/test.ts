897a9fd941bbe6371e914fcafb4b77996f972939
willy <willysliang>
2024-01-09 21:11:46 +0800

    docs(前端算法): [数据结构] 删除链表中的选定节点/重复节点

--- src/entries/index/test.ts
+++ src/entries/index/test.ts
@@ -27,75 +27,45 @@ export class ListNode<T = number> implements ILinkedNode<T> {
 }
 
 /**
- * 约瑟夫环问题
- * @param {number} n 参与游戏的总人数
- * @param {number} m 每报数到 m 的人出列
- * @returns {number} 存活的人的编号（仅能剩下一个人）
+ * 用来删除重复链表节点
+ * @param {ICurListNode<T>} head - 链表的头节点
+ * @returns {ICurListNode<T>} 删除重复节点后的链表头节点
  */
-function josephus(n: number, m: number): number {
-  /** 边界条件 */
-  if (n < 1 || m < 1) return -1
+function deleteDuplicatesNode<T = number>(
+  head: ICurListNode<T>,
+): ICurListNode<T> {
+  /** 如果是链表为空 或 链表只有一个节点，则直接返回 */
+  if (head === null || head.next === null) return head
 
-  const head: ICurListNode = new ListNode(1)
-  let current: ICurListNode = head
+  const listNodeMap = new Map<T, boolean>()
+  let currentNode: ICurListNode<T> = head
+  let prev: ICurListNode<T> = null
 
-  // 循环生成长度为 n 的链表
-  for (let i = 2; i <= n; i++) {
-    current.next = new ListNode(i)
-    current = current.next
-  }
-
-  // 构成环状链表
-  current.next = head
-
-  /** 指针的前一个节点 */
-  let prev: ICurListNode = current
-  /** 计数值 */
-  let count: number = 0
-  /** 当前指针 */
-  let cur: ICurListNode = head
-
-  /** 直到只有一个节点为止 */
-  while (cur !== cur.next) {
-    if (++count === m) {
-      // 每当报数到达 m 时，就将当前节点从链表中移除（通过修改前一个节点的指针），并将计数器重置为 0
-      prev.next = cur.next
-      count = 0
+  while (currentNode !== null) {
+    if (listNodeMap.has(currentNode.value)) {
+      // 删除重复节点
+      prev!.next = currentNode.next
     } else {
-      // 如果报数未达到 m，就简单地将当前节点指针移动到下一个节点。
-      prev = cur
+      listNodeMap.set(currentNode.value, true)
+      prev = currentNode
     }
 
-    // 指针后移
-    cur = cur.next as ListNode<number>
+    currentNode = currentNode.next
   }
 
-  return cur.value
+  return head
 }
 
-/**
- * 约瑟夫环问题
- * @param {number} n 参与游戏的总人数
- * @param {number} m 每报数到 m 的人出列
- * @returns {number} 存活的人的编号（可剩下多个人）
- */
-function josephusAsArr(n, m) {
-  const persons = Array.from({ length: n }).map((_, idx) => idx + 1)
+/** 测试用例 */
+// 创建链表
+const node1 = new ListNode(1)
+const node2 = new ListNode(2)
+const node3 = new ListNode(2)
+const node4 = new ListNode(3)
+node1.next = node2
+node2.next = node3
+node3.next = node4
 
-  let index = 0
-  //   while (persons.length > 1) {
-  while (persons.length >= m) {
-    index = ((index + m) % persons.length) - 1
-    if (index >= 0) {
-      persons.splice(index, 1)
-    } else {
-      persons.splice(persons.length - 1, 1)
-      index = 0
-    }
-  }
-
-  return persons
-}
-
-/** 测试代码 */
-console.log(josephus(11, 3), josephusAsArr(11, 3)) // 输出：4
+// 调用函数删除重复节点
+const newHead = deleteDuplicatesNode(node1)
+console.log(newHead)
