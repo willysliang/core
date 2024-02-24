7ce008cf03873ef17b7c5fd63eaf5a3a71bcf083
willy <willysliang@qq.com>
2024-02-24 10:55:30 +0800

    perf(server): 优化封装MongooseConnect类

--- RECORD.md
+++ RECORD.md
@@ -25,6 +25,9 @@ Description: 日志记录
 
 # 更新记录
 
+## 2024年2月24日10点53分
+  - perf(server): 优化封装MongooseConnect类
+
 ## 2024年2月23日18点54分
   - feat(utils): 添加通用方法子模块
   - feat(server): webPush消息推送、调整调用通用方法、优化 public/webPush 方法
--- packages/server/src/config/mongooseDB.connect.ts
+++ packages/server/src/config/mongooseDB.connect.ts
@@ -1,15 +1,30 @@
 /**
  * @ Author: willy
  * @ CreateTime: 2024-02-20 17:46:47
- * @ Modifier: willy
- * @ ModifierTime: 2024-02-21 14:19:14
+ * @ Modifier: willysliang
+ * @ ModifierTime: 2024-02-24 10:23:51
  * @ Description: 数据库连接
  */
 
-import mongoose, { Connection } from 'mongoose'
+import mongoose, { Connection, ConnectOptions } from 'mongoose'
 import { mongodbConfig } from './db.config'
 
 /**
+ * @class MongooseConnect
+ * mongoose 链接类
+ */
+class MongooseConnect {
+  public mongooseUrl: string
+  public mongooseOptions: ConnectOptions
+  public mongooseDBConfig: Record<string, any>
+
+  constructor(mongooseDBConfig) {
+    this.mongooseDBConfig = mongooseDBConfig
+    this.mongooseUrl = this.getMongoUrl(this.mongooseDBConfig)
+    this.mongooseOptions = this.getMongoOptions(this.mongooseDBConfig)
+  }
+
+  /**
  * mongoose url 字符串拼接
  * @description 连接字符串类型
    副本集模式无认证：mongodb://member1Host:member1Port,member2Host:member2Port,.../dbName
@@ -18,82 +33,83 @@ import { mongodbConfig } from './db.config'
    单机模式有认证：mongodb://user:password@host:port/dbName?authSource=admin
  * @returns {string}
  */
-export function getMongoUrl(): string {
-  // 前缀拼接
-  let mongoUrl = 'mongodb://'
+  getMongoUrl(mongodbConfig): string {
+    // 前缀拼接
+    let mongoUrl = 'mongodb://'
 
-  const replicaSet = mongodbConfig.replicaSet
-  if (replicaSet.name) {
-    // 副本集模式
-    const members = replicaSet.members
-    const memberInfos = members.map((member) => `${member.host}:${member.port}`)
-    mongoUrl += memberInfos.join(',')
-  } else {
-    // 单机模式
-    const hostAndPort = `${mongodbConfig.host}:${mongodbConfig.port}`
-    mongoUrl += mongodbConfig.user
-      ? `${mongodbConfig.user}:${mongodbConfig.password}@${hostAndPort}`
-      : hostAndPort
+    const replicaSet = mongodbConfig.replicaSet
+    if (replicaSet.name) {
+      // 副本集模式
+      const members = replicaSet.members
+      const memberInfos = members.map(
+        (member) => `${member.host}:${member.port}`,
+      )
+      mongoUrl += memberInfos.join(',')
+    } else {
+      // 单机模式
+      const hostAndPort = `${mongodbConfig.host}:${mongodbConfig.port}`
+      mongoUrl += mongodbConfig.user
+        ? `${mongodbConfig.user}:${mongodbConfig.password}@${hostAndPort}`
+        : hostAndPort
+    }
+
+    // 后缀拼接
+    const dbName = mongodbConfig.database
+    mongoUrl += `/${dbName}`
+    if (mongodbConfig.user) {
+      mongoUrl += `?authSource=${mongodbConfig.authSource}`
+    }
+
+    return mongoUrl
   }
 
-  // 后缀拼接
-  const dbName = mongodbConfig.database
-  mongoUrl += `/${dbName}`
-  if (mongodbConfig.user) {
-    mongoUrl += `?authSource=${mongodbConfig.authSource}`
+  /**
+   * 配置 mongoDB options
+   */
+  getMongoOptions(mongodbConfig): ConnectOptions {
+    const options: ConnectOptions = {}
+    if (mongodbConfig.user) options.user = mongodbConfig.user
+    if (mongodbConfig.password) options.pass = mongodbConfig.password
+    if (mongodbConfig.replicaSet.name)
+      options.replicaSet = mongodbConfig.replicaSet.name
+    return options
   }
 
-  return mongoUrl
-}
+  connectDB() {
+    const mongoClient: Connection = mongoose.createConnection(
+      this.mongooseUrl,
+      this.mongooseOptions,
+    )
 
-/**
- * 配置 mongoDB options
- */
-export function getMongoOptions() {
-  const options = {
-    // poolSize: 5, // 连接池中维护的连接数
-    // reconnectTries: Number.MAX_VALUE,
-    // keepAlive: 120,
-  } as any
-  if (mongodbConfig.user) options.user = mongodbConfig.user
-  if (mongodbConfig.password) options.pass = mongodbConfig.password
-  if (mongodbConfig.replicaSet.name)
-    options.replicaSet = mongodbConfig.replicaSet.name
-  return options
-}
+    /**
+     * Mongo 连接成功回调
+     */
+    mongoClient.on('connected', () => {
+      console.log('Mongoose connected to ' + this.getMongoUrl)
+    })
 
-export const connectDB = () => {
-  const mongoClient: Connection = mongoose.createConnection(
-    getMongoUrl(),
-    getMongoOptions(),
-  )
+    /**
+     * Mongo 连接失败回调
+     */
+    mongoClient.on('error', (err) => {
+      console.log('Mongoose connection error: ' + this.getMongoUrl + err)
+    })
+    /**
+     * Mongo 关闭连接回调
+     */
+    mongoClient.on('disconnected', () => {
+      console.log('Mongoose disconnected')
+    })
 
-  /**
-   * Mongo 连接成功回调
-   */
-  mongoClient.on('connected', () => {
-    console.log('Mongoose connected to ' + getMongoUrl())
-  })
-  /**
-   * Mongo 连接失败回调
-   */
-  mongoClient.on('error', (err) => {
-    console.log('Mongoose connection error: ' + getMongoUrl() + err)
-  })
-  /**
-   * Mongo 关闭连接回调
-   */
-  mongoClient.on('disconnected', () => {
-    console.log('Mongoose disconnected')
-  })
+    // 关闭 Mongo 连接 (还需要通过调用该函数才能触发)
+    const close = mongoClient.close
 
-  // 关闭 Mongo 连接 (还需要通过调用该函数才能触发)
-  const close = mongoClient.close
-
-  return {
-    mongoClient,
-    close,
+    return {
+      mongoClient,
+      close,
+    }
   }
 }
 
-export const connectPool = connectDB()
+export const mongooseConnect = new MongooseConnect(mongodbConfig)
+export const connectPool = mongooseConnect.connectDB()
