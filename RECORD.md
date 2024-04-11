b03a7141e9a5d7f6a28e718c1fe0242501eb97d0
willy <willysliang@qq.com>
2024-04-11 15:30:39 +0800

    feat(utils): [http] [FetchRequest] 封装fetch网络请求

--- RECORD.md
+++ RECORD.md
@@ -26,6 +26,9 @@ Description: 日志记录
 # 更新记录
 
 
+## 2024年4月11日
+  - feat(utils): [http] [FetchRequest] 封装fetch网络请求
+
 ## 2024年3月29日
   - style(all): [editorconfig] 配置文件格式化
 
--- packages/utils/src/utils/index.ts
+++ packages/utils/src/utils/index.ts
@@ -2,7 +2,7 @@
  * @ Author: willy
  * @ CreateTime: 2024-02-23 14:50:31
  * @ Modifier: willy
- * @ ModifierTime: 2024-03-06 12:57:47
+ * @ ModifierTime: 2024-04-10 20:32:03
  * @ Description: 辅助函数或通用方法入口
  */
 
@@ -10,3 +10,4 @@ export * from './app/index'
 export * from './common/index'
 export * from './dom/index'
 export * from './file/index'
+export * from './http/index'
