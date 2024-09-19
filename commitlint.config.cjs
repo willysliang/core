9842882ef8d4463445a77006c42ef7c9740bc53d
willy <willysliang@qq.com>
2024-09-19 15:28:29 +0800

    chore: [commitlint] 增加perf提交规范

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
         'docs', // 文档（documentation）
         'style', // 格式（不影响代码运行的变动）
         'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+        'perf', // 优化
         'test', // 增加测试
         'chore', // 构建过程或辅助工具的变动
         'revert', // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
