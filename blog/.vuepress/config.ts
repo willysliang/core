6ef503eddfadc5f3f3584c9d4ff8834ff8fa043f
willy <willysliang@qq.com>
2025-01-03 16:20:47 +0800

    perf(blog): 优化blog的icon显示，并移除非必要img

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @ Author: willy
  * @ CreateTime: 2024-02-26 19:19:37
  * @ Modifier: willysliang
- * @ ModifierTime: 2024-03-12 23:02:57
+ * @ ModifierTime: 2025-01-03 16:10:39
  * @ Description: 配置文件
  */
 
@@ -10,10 +10,12 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defineUserConfig } from 'vuepress'
 import theme from './theme/defaultTheme'
 
+const BASEURL = '/static_blog/'
+
 export default defineUserConfig({
   head: [
     // 引入你的图标文件
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['link', { rel: 'icon', href: `${BASEURL}logo__icon--default.png` }],
   ],
 
   bundler: viteBundler({
@@ -22,7 +24,7 @@ export default defineUserConfig({
   }),
   theme,
 
-  base: '/static_blog/',
+  base: BASEURL,
   port: 8080,
 
   lang: 'zh-CN',
